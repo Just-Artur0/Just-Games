6743d1075afe29c138bb6d262f9a665fc19d93b1
Just-Artur0 <okodobre@gmail.com>
2025-11-30 19:00:32 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import pygame
 from os.path import join
-pygame.init()
-pygame.font.init()
-pygame.mixer.init()
-pygame.display.init()
 font_path = join("Fonts", "Game Of Squids.ttf")
 if __name__ == "__main__":
     from menus import mainmenu
